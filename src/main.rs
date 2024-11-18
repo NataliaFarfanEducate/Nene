8addf0cd73a0c6a760092161b6760632ff840cd4
Canquito <jbejarano@uniempresarial.edu.co>
2024-11-17 19:26:01 -0500

    New Change

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-    println!("\nHello, classmate!");
+
+    let name = "Natalia";
+    let age = 17;
+
+    println!("\nHello, {}!", name);
+    println!("{} is {} years old",name, age);
+
+    //println!("\nHello, classmate!");
     println!("\nThis is a new project using Rust");
     println!("\nI hope you enjoy it!\n");
 }
